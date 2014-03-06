985f5b7f491126de27b6a5c076a318c96ed7e99e
Isabell Long <isabell.long@digital.cabinet-office.gov.uk>
2014-03-06 12:20:07 +0000

    Name the variables correctly - really fix the dashboard display

--- jobs/org_status.rb
+++ jobs/org_status.rb
@@ -7,5 +7,5 @@ SCHEDULER.every '15m', :first_in => 0 do |job|
   done = XmlSimple.xml_in(RestClient.get("#{spreadsheet_root}/od6/public/values/R1C6"))['content']['content']
   todo = XmlSimple.xml_in(RestClient.get("#{spreadsheet_root}/od6/public/values/R1C8"))['content']['content']
   send_event('orgs_done', { done: done })
-  send_event('orgs_done', { todo: todo })
+  send_event('orgs_todo', { todo: todo })
 end
