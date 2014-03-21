608f3745360313e0846786f25fdd44822a6abdad
Isabell Long <isabell.long@digital.cabinet-office.gov.uk>
2014-03-21 12:16:30 +0000

    Further refactoring of the spreadsheet data retrieval code

--- jobs/org_top_ten.rb
+++ jobs/org_top_ten.rb
@@ -2,11 +2,19 @@ require 'rest_client'
 require 'xmlsimple'
 require 'titleize'
 
-spreadsheet_root = "https://spreadsheets.google.com/feeds/cells/#{ENV['main_spreadsheet_key']}"
-# Worksheet three for the list of top 10 agencies.
-spreadsheet_end_url = "#{spreadsheet_root}/3/public/values"
-
 org_top_ten = Hash.new({ value: 0 })
+
+def get_spreadsheet_xml(row, column)
+  # Worksheet three for the list of top 10 agencies.
+  spreadsheet_url = "https://spreadsheets.google.com/feeds/cells/#{ENV['main_spreadsheet_key']}/3/public/values"
+
+  data = Hash.new()
+  (0..4).each do |i|
+    data[i] = XmlSimple.xml_in(RestClient.get("#{spreadsheet_url}/R#{row}C#{column}"))['content']['content']
+    column += 1
+  end
+  data.values
+end
 
 def abbreviate_status(status)
   # Construct abbreviations for one long status from the spreadsheet.
@@ -22,13 +30,16 @@ end
 SCHEDULER.every '2h', :first_in => 0 do |job|
   row = 2
   org_number = 0
+
   (0..9).each do
-    org = XmlSimple.xml_in(RestClient.get("#{spreadsheet_end_url}/R#{row}C1"))['content']['content']
-    status = abbreviate_status(XmlSimple.xml_in(RestClient.get("#{spreadsheet_end_url}/R#{row}C2"))['content']['content'])
-    # Ignore the potentially present "- live" in the target month.
-    target = XmlSimple.xml_in(RestClient.get("#{spreadsheet_end_url}/R#{row}C3"))['content']['content'].strip.split("-")[0]
-    rag = XmlSimple.xml_in(RestClient.get("#{spreadsheet_end_url}/R#{row}C4"))['content']['content']
-    org_top_ten[org_number] = { label: org, rag: rag, status: status, target: target }
+    org_values = get_spreadsheet_xml(row, 1)
+
+    org_top_ten[org_number] = {
+                                label: org_values[0],
+                                status: abbreviate_status(org_values[1]),
+                                target: org_values[2].strip.split("-")[0],
+                                rag: org_values[3]
+                              }
     row += 1
     org_number += 1
   end
