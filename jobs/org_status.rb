dfab04eb14147d44dd9cfb8d5043c1706a8d3178
Isabell Long <isabell.long@digital.cabinet-office.gov.uk>
2014-04-06 19:10:09 +0100

    Make `spreadsheet_root` a constant and add more of the URL to it

--- jobs/org_status.rb
+++ jobs/org_status.rb
@@ -1,11 +1,11 @@
 require 'rest_client'
 require 'xmlsimple'
 
-spreadsheet_root = "https://spreadsheets.google.com/feeds/cells/#{ENV['main_spreadsheet_key']}"
+SPREADSHEET_ROOT = "https://spreadsheets.google.com/feeds/cells/#{ENV['main_spreadsheet_key']}/od6/public/values"
 
 SCHEDULER.every '15m', :first_in => 0 do |job|
-  done = XmlSimple.xml_in(RestClient.get("#{spreadsheet_root}/od6/public/values/R1C6"))['content']['content']
-  todo = XmlSimple.xml_in(RestClient.get("#{spreadsheet_root}/od6/public/values/R1C8"))['content']['content']
+  done = XmlSimple.xml_in(RestClient.get("#{SPREADSHEET_ROOT}/R1C6"))['content']['content']
+  todo = XmlSimple.xml_in(RestClient.get("#{SPREADSHEET_ROOT}/R1C8"))['content']['content']
   send_event('orgs_done', { done: done })
   send_event('orgs_todo', { todo: todo })
 end
