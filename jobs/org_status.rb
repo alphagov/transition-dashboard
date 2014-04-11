4f1d7b778aa27262e348373c623d7ed4b6f94808
Isabell Long <isabell.long@digital.cabinet-office.gov.uk>
2014-04-11 15:47:22 +0100

    Fix #N/A sporadically appearing in the agency number widgets
    - This was caused by Google Spreadsheets running the command to
      import data from the transition spreadsheet and not getting data.
      This made `#N/A` appear for unpredictable lengths of time,
      unpredictably, so this hopefully successfully deals with it by
      querying the spreadsheet every two seconds until the todo and done
      values are no longer `#N/A`, or another variant `#VALUE!`.

--- jobs/org_status.rb
+++ jobs/org_status.rb
@@ -4,8 +4,12 @@ require 'xmlsimple'
 SPREADSHEET_ROOT = "https://spreadsheets.google.com/feeds/cells/#{ENV['main_spreadsheet_key']}/od6/public/values"
 
 SCHEDULER.every '15m', :first_in => 0 do |job|
-  done = XmlSimple.xml_in(RestClient.get("#{SPREADSHEET_ROOT}/R1C6"))['content']['content']
-  todo = XmlSimple.xml_in(RestClient.get("#{SPREADSHEET_ROOT}/R1C8"))['content']['content']
+  begin
+    done = XmlSimple.xml_in(RestClient.get("#{SPREADSHEET_ROOT}/R1C6"))['content']['content']
+    todo = XmlSimple.xml_in(RestClient.get("#{SPREADSHEET_ROOT}/R1C8"))['content']['content']
+    sleep(2)
+  end until done != '#N/A' && done != '#VALUE!' && todo != "#N/A" && todo != '#VALUE!'
+
   send_event('orgs_done', { done: done })
   send_event('orgs_todo', { todo: todo })
 end
